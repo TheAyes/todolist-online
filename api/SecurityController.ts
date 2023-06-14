59deaf6c793c411b78c45d98f5ca67c226023525
Justin Schildt <Just.schildt@gmail.com>
2023-06-14 09:01:57 +0200

    Loosen up safety pattern

--- api/SecurityController.ts
+++ api/SecurityController.ts
@@ -1,7 +1,7 @@
 import {NextFunction, Request, Response} from "express";
 
 export const sanitizeBody = (req: Request, res: Response, next: NextFunction) => {
-	const safetyPattern = /^[a-zA-Z0-9\._-]*$/;
+	const safetyPattern = /^[a-zA-Z0-9_ .!?\-@&\u00C0-\u00D6\u00D8-\u00F6\u00F8-\u00FF ]*$/;
 
 	if (req.body) {
 		for (const key in req.body) {
